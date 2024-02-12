cc15d1919a02fd5a012232f9a2794899591ddd7d
RaulGonzalezLeon <raul.gonzalez.leon.04@gmail.com>
2024-02-12 20:51:14 +0100

    recycler

---
+++ app/src/main/java/com/example/proyectofinalmodulo/InsertarAnimales.kt
@@ -0,0 +1,11 @@
+package com.example.proyectofinalmodulo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InsertarAnimales : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insertar_animales)
+    }
+}
