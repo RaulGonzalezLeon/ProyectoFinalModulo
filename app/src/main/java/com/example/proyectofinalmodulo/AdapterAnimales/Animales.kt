628809f82e7f5944c12824fe3470309c314a37e6
RaulGonzalezLeon <raul.gonzalez.leon.04@gmail.com>
2024-06-06 18:30:01 +0200

    cambios

--- app/src/main/java/com/example/proyectofinalmodulo/AdapterAnimales/Animales.kt
+++ app/src/main/java/com/example/proyectofinalmodulo/AdapterAnimales/Animales.kt
@@ -1,3 +1,8 @@
 package com.example.proyectofinalmodulo.AdapterAnimales
 
-data class Animales(val numeroChip:String="",val nombre:String="", val raza:String="", val alimentacion:String="", val anioNacimiento:String="", val imagenUrl: String = "")
+data class Animales(val numeroChip:String="",
+                    val nombre:String="",
+                    val raza:String="",
+                    val alimentacion:String="",
+                    val anioNacimiento:String="",
+                    val imagenUrl: String = "")
